5a51d021bddead6125317260fbe189093b2f472d
João Marcos Vilar <joaomarcossbj172001@gmail.com>
2025-07-29 14:26:28 -0300

    fix gpio num

--- main/hardware/humidity/humidity.h
+++ main/hardware/humidity/humidity.h
@@ -4,8 +4,8 @@
 #define HUMIDITY_SENSOR_ADDR AHT_I2C_ADDRESS_GND
 #define HUMIDITY_I2C_PORT I2C_NUM_0
 
-#define HUMIDITY_I2C_SDA_GPIO GPIO_NUM_21
-#define HUMIDITY_I2C_SCL_GPIO GPIO_NUM_22
+#define HUMIDITY_I2C_SDA_GPIO GPIO_NUM_8
+#define HUMIDITY_I2C_SCL_GPIO GPIO_NUM_9
 
 esp_err_t humidity_init(void);
 float humidity_get(void);
